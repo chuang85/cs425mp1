e251e330fa7b96e40318729a316e044937ee1a2f
chuang85 <chuang85@illinois.edu>
2014-02-24 02:36:49 -0600

    some refactor

--- cs425mp1/src/client/ClientMain.java
+++ cs425mp1/src/client/ClientMain.java
@@ -1,9 +1,12 @@
 package client;
 
 public class ClientMain {
+	
+	public static final int PORT = 6666;
+	
 	public static void main(String[] args)
 	{
-		Client my_client = new Client("localhost", 6666);
+		Client my_client = new Client("localhost", PORT);
 		my_client.connect();
 		
 	}
